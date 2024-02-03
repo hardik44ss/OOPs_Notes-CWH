c1d213c66b2b0ac9aca483fb196593a66fc5826c
Harbansh <142929609+hardik44ss@users.noreply.github.com>
2024-02-03 18:25:56 +0530

    Create 25_ambiguityreso.cpp

---
+++ 25_ambiguityreso.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+class Base1 {
+    public:
+        void samefn(){
+            cout<<"\nVoid function of Base 1"<<endl;
+        }
+};
+class Base2 {
+    public:
+        void samefn(){
+            cout<<"Void function of Base 2";
+        }
+};
+class Derived : public Base1 , public Base2 {
+    public:
+        void samefn(){
+            Base1::samefn(); // THis is called as ambiguity resolution
+            //WHen two fuction is same in the base blass then we have to specify that which functin we want to use
+        }
+}; 
+int main () 
+{
+    Derived d;
+    d.samefn();
+    return 0;
+}
